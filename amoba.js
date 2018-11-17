c9bd6c706fe251d6f8d4a4aa28d3ce9b332075c9
Andris <pavlicz.andris@gmail.com>
2018-11-17 01:30:29 +0100

    first commit

---
+++ amoba.js
@@ -0,0 +1,160 @@
+var canvas = document.getElementById('canvas'),
+    playerSpan = document.getElementById('player'),
+    ctx = canvas.getContext("2d"),
+    tileWidth = 40,
+    tilePaddingO = 8,
+    tilePaddingX = 5,
+    isBusy = false,
+    alpha = 0,
+    delta = 0.05,
+    numTiles = canvas.width * canvas.height / tileWidth,
+    startingTile, endTile, player, turn, tiles,
+    players = Object.freeze({ default: "", X: "X", O: "O" }),
+    colors = Object.freeze({ brown: "#654321", red: "#8B0000", blue: "#000080" });
+ctx.lineWidth = 1.5;
+
+resetGame();
+drawGrid();
+
+function resetGame() {
+    setPlayer(players.X);
+    turn = 0;
+
+    tiles = new Array(canvas.width / tileWidth);
+    for (let i = 0; i < tiles.length; i++) { tiles[i] = new Array(canvas.height / tileWidth).fill(players.default); }
+    canvas.onclick = function (e) { put((e.offsetX / tileWidth) | 0, (e.offsetY / tileWidth) | 0); }
+}
+
+function drawGrid() {
+    ctx.fillStyle = colors.brown;
+    for (let i = 1; i < tiles.length; i++) {
+        ctx.fillRect(i * tileWidth, 0, 1.5, canvas.height);
+        ctx.fillRect(0, i * tileWidth, canvas.width, 1.5);
+    }
+}
+
+function put(xPos, yPos) {
+    if (isBusy) return;
+    if (tiles[yPos][xPos] === players.X || tiles[yPos][xPos] === players.O) return;
+    tiles[yPos][xPos] = player;
+    player === players.X ? drawX(xPos * tileWidth, yPos * tileWidth) : drawO(xPos * tileWidth, yPos * tileWidth);
+    if (checkWinner(xPos, yPos)) endGame(player);
+    else togglePlayer();
+    if (++turn === numTiles) endGame(null);
+}
+
+function setPlayer(value) {
+    player = value;
+    playerSpan.innerText = player;
+}
+
+function togglePlayer() { player === players.X ? setPlayer(players.O) : setPlayer(players.X); }
+
+function drawX(x, y) {
+    isBusy = true;
+    ctx.strokeStyle = colors.red;
+    (function draw() {
+        ctx.clearRect(x + 2, y + 2, tileWidth - 2, tileWidth - 2);
+        ctx.globalAlpha = alpha;
+        alpha += delta;
+        ctx.beginPath();
+        ctx.moveTo(x + tilePaddingX, y + tilePaddingX);
+        ctx.lineTo(x + tileWidth - tilePaddingX, y + tileWidth - tilePaddingX);
+        ctx.moveTo(x + tileWidth - tilePaddingX, y + tilePaddingX);
+        ctx.lineTo(x + tilePaddingX, y + tileWidth - tilePaddingX);
+        ctx.stroke();
+        if (alpha < 1) requestAnimationFrame(draw);
+        else {
+            alpha = 0;
+            isBusy = false;
+            return;
+        }
+    })();
+
+}
+
+function drawO(x, y) {
+    isBusy = true;
+    ctx.strokeStyle = colors.blue;
+    (function draw() {
+        ctx.clearRect(x + 2, y + 2, tileWidth - 2, tileWidth - 2);
+        ctx.globalAlpha = alpha;
+        alpha += delta;
+        ctx.beginPath();
+        ctx.arc(x + tileWidth / 2, y + tileWidth / 2, (tileWidth - tilePaddingO) / 2, 0, 2 * Math.PI);
+        ctx.stroke();
+        if (alpha < 1) requestAnimationFrame(draw);
+        else {
+            alpha = 0;
+            isBusy = false;
+            return;
+        }
+    })();
+}
+
+function checkWinner(x, y) {
+    return (
+        check(x, y, add, noop)
+        || check(x, y, noop, add)
+        || check(x, y, add, add)
+        || check(x, y, add, sub)
+    );
+}
+
+function add(a, b) { return a + b; }
+function noop(a, _b) { return a; }
+function sub(a, b) { return a - b; }
+
+function check(x, y, funcX, funcY) {
+    let score = 0;
+    for (let i = - 4; i <= 4; i++) {
+        let xCurr = funcX(x, i);
+        let yCurr = funcY(y, i);
+        let tileCurr = { x: xCurr, y: yCurr };
+        if (isOut(xCurr, yCurr)) continue;
+        if (tiles[yCurr][xCurr] === player) {
+            if (score === 0) startingTile = tileCurr;
+            if (++score === 5) {
+                endTile = tileCurr;
+                return true;
+            }
+        } else score = 0;
+    }
+    return false;
+}
+
+function isOut(x, y) {
+    return (
+        x < 0
+        || y < 0
+        || x >= canvas.width / tileWidth
+        || y >= canvas.height / tileWidth
+    );
+}
+
+function endGame(winner) {
+    canvas.onclick = null;
+    if (winner) { setTimeout(() => showWinnerTiles(winner), 300); }
+}
+
+function showWinnerTiles() {
+    subValues = { x: (startingTile.x - endTile.x) / 4, y: (startingTile.y - endTile.y) / 4 };
+    for (let i = 0; i < 5; i++) {
+        setTimeout(() => colorTile((startingTile.x - subValues.x * i) * tileWidth, (startingTile.y - subValues.y * i) * tileWidth), 100 * i);
+    }
+}
+
+function colorTile(x, y) {
+    ctx.fillStyle = player === players.X ? colors.red : colors.blue;
+    let a = 0;
+    (function draw() {
+        ctx.globalAlpha = a;
+        a += 0.01;
+        ctx.fillRect(x, y, tileWidth, tileWidth);
+        if (a < 0.15) requestAnimationFrame(draw);
+        else {
+            a = 0;
+            return;
+        }
+    })();
+}
